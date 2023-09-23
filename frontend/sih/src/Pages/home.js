5d3bb591128781d1ac2c3a472a959a646679344c
zeusopera9 <zaidalimerchant@yahoo.com>
2023-09-23 10:23:32 +0530

    Upload

--- frontend/sih/src/Pages/home.js
+++ frontend/sih/src/Pages/home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "../Components/Header";
+import VideoUpload from "../Components/Upload";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      
+      <VideoUpload />
     </div>
   );
 }
