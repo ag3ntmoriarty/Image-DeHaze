fb2eceff28a691952d7039865fcb6a7c62333f09
zeusopera9 <zaidalimerchant@yahoo.com>
2023-09-23 08:58:36 +0530

    Initial

---
+++ frontend/sih/src/Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <h1>PixelEncoders SIH 2023</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
