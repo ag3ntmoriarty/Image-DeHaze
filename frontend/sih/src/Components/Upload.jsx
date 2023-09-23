c330b5c66ac9f9115ff8025958ac2cf7b960acec
atharva <atharvak.8023@gmail.com>
2023-09-23 22:30:24 +0530

    upload.jsx

--- frontend/sih/src/Components/Upload.jsx
+++ frontend/sih/src/Components/Upload.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000'; 
-
 export default function VideoUpload() {
   const [selectedFile, setSelectedFile] = useState(null);
 
@@ -13,37 +11,30 @@ export default function VideoUpload() {
 
   const handleUpload = async () => {
     if (!selectedFile) return;
-  
+
     console.log('Selected file:', selectedFile); // Log the selected file
-  
+
     const formData = new FormData();
     formData.append('video', selectedFile);
-  
+
     try {
       const response = await axios.post('http://localhost:3001/api/upload-video', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       });
-  
+
       console.log('Upload success:', response.data);
     } catch (error) {
       console.error('Upload error:', error);
     }
   };
-  
+
 
   return (
     <div>
       <input type="file" accept=".mp4" onChange={handleFileChange} />
       <button type="button" onClick={handleUpload}>Upload Video</button>
-      <form action={`${API_URL}/upload`} method="post" encType="multipart/form-data">
-        <input type="file" accept=".mp4" onChange={handleFileChange} />
-        <button type="button" onClick={handleUpload}>
-          Upload Video
-        </button>
-        <video ref={videoRef} controls autoPlay></video>
-      </form>
     </div>
   );
 }
