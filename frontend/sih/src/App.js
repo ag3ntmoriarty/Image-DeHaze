7093ff428b841655c438bc423a2a7858abb298f2
atharva <atharvak.8023@gmail.com>
2023-09-24 04:06:52 +0530

    ho gaya?

--- frontend/sih/src/App.js
+++ frontend/sih/src/App.js
@@ -2,18 +2,33 @@ import './App.css';
 import VideoUpload from './Components/Upload';
 import Home from './Pages/home';
 import Display from './Components/Display';
+import { useState } from 'react';
 
 function App() {
-  const videoSrc = '../../../backend/uploads/results/result.mp4';
+  const [videoSrc, setVideoSrc] = useState('');
+
+  // Fetch the URL of the processed video from your Express.js backend
+  // and set it as the 'videoSrc' state variable
+  const getVideoSrc = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/results/result.mp4');
+      const data = await response.json();
+      console.log('Video src:', data.outputFilePath);
+      setVideoSrc(data.outputFilePath);
+    } catch (error) {
+      console.error('Error fetching video source:', error);
+    }
+  };
+
   return (
     <div>
       <Home />
-      <hr />
-      <h1>
-        Video Display
-      </h1>
-      <Display videoSrc={videoSrc}/>
+      {/* <VideoUpload getVideoSrc={getVideoSrc} /> Pass the 'getVideoSrc' function to the VideoUpload component */}
+      <Display 
+      videoSrc="http://localhost:3001/results/result.mp4"
+      />
     </div>
   );
 }
+
 export default App;
--- frontend/sih/src/Components/Display.jsx
+++ frontend/sih/src/Components/Display.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 export default function Display({ videoSrc }) {
   return (
     <div>
       <video controls width="400">
         <source src={videoSrc} type="video/mp4" />
-        Your browser does not support the video tag
+        Your browser does not support the video tag.
       </video>
     </div>
-  )
+  );
 }
