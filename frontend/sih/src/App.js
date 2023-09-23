64369dd670a4ab7336a0b38957aee7067947d270
zeusopera9 <zaidalimerchant@yahoo.com>
2023-09-24 01:02:17 +0530

    OP video

--- frontend/sih/src/App.js
+++ frontend/sih/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import VideoUpload from './Components/Upload';
 import Home from './Pages/home';
+import Display from './Components/Display';
 
 function App() {
+  const videoSrc = '../../../backend/uploads/results/result.mp4';
   return (
     <div>
       <Home />
-      
+      <hr />
+      <h1>
+        Video Display
+      </h1>
+      <Display videoSrc={videoSrc}/>
     </div>
   );
 }
