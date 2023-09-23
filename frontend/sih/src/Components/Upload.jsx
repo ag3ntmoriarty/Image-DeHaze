e8a442e2f1f085dda1d2c852e7c44d5e48693544
atharva <atharvak.8023@gmail.com>
2023-09-23 14:47:05 +0530

    ho gaya

--- frontend/sih/src/Components/Upload.jsx
+++ frontend/sih/src/Components/Upload.jsx
@@ -1,13 +1,15 @@
-// VideoUploader.js
-
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
 
-function VideoUploader() {
+const API_URL = 'http://localhost:3001'; // Replace with your actual backend URL
+
+export default function VideoUpload() {
   const [selectedFile, setSelectedFile] = useState(null);
+  const videoRef = useRef(null);
 
   const handleFileChange = (e) => {
-    setSelectedFile(e.target.files[0]);
+    const file = e.target.files[0];
+    setSelectedFile(file);
   };
 
   const handleUpload = async () => {
@@ -17,24 +19,31 @@ function VideoUploader() {
     formData.append('video', selectedFile);
 
     try {
-      const response = await axios.post('http://localhost:5000/upload-video', formData, {
+      const response = await axios.post(`${API_URL}/api/upload-video`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       });
 
+      // Handle the response from the server (display the video URL)
       console.log('Upload success:', response.data);
+
+      // Set the video source and play it
+      if (videoRef.current && response.data.video_url) {
+        videoRef.current.src = `${API_URL}${response.data.video_url}`;
+        videoRef.current.play();
+      }
     } catch (error) {
+      // Handle errors (e.g., display an error message)
       console.error('Upload error:', error);
     }
   };
 
   return (
     <div>
-      <input type="file" accept="video/*" onChange={handleFileChange} />
+      <input type="file" accept=".mp4" onChange={handleFileChange} />
       <button onClick={handleUpload}>Upload Video</button>
+      <video ref={videoRef} controls autoPlay></video>
     </div>
   );
 }
-
-export default VideoUploader;
