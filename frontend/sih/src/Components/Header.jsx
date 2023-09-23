87c1fa8fffcf05780adf02d017ad228c1645c2f9
Rylan Lewis <rylanlewis045@gmail.com>
2023-09-23 21:52:53 +0530

    css

--- frontend/sih/src/Components/Header.jsx
+++ frontend/sih/src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 // import VideoUpload from './Upload';
 // import LiveFeed from './Live';
 import "./Header.css";
@@ -16,4 +16,62 @@ export default function Header() {
 
     </div>
   )
+}*/
+
+import React from 'react';
+// import VideoUpload from './Upload';
+// import LiveFeed from './Live';
+import "./Header.css";
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import { useState } from 'react';
+
+export default function Content({ setSelectedOption }) {
+  const handleSwitchChange = (event) => {
+    const option = event.target.name; // Get the name attribute from the FormControlLabel
+    setSelectedOption(option);
+  };
+
+  // const[show,setShow]=useState[false];
+
+  return (
+    <div id="contain">
+      <div id="header">
+        <h2>PixelEncoders SIH 2023</h2>
+        <p>Team #5: </p>
+        <p><strong>Problem Statement:</strong> Design and development of AI-ML based intelligent de-smoking/hazing
+        algorithm for reproducing the real time video of the area under fire specifically for indoor fire hazards to
+        aid the rescue operation</p>
+      </div>
+      <div id="options">
+        <strong>
+          <h2>
+            <i>Options Offered:</i>
+          </h2>
+        </strong>
+
+        <div className="content-item">
+          <div className='left'>
+            <p>Video Upload</p>
+          </div>
+          <div className='right'>
+            <FormControlLabel
+              control={<Switch defaultChecked={false} name="live" onChange={handleSwitchChange} />}
+              label="Live Feed"
+            />
+          </div>
+        </div>
+        
+
+        {/* <div className="content-item">
+          <FormControlLabel
+            control={<Switch defaultChecked={false} name="video" onChange={handleSwitchChange} />}
+            label="Video Upload"
+          />
+        </div> */}
+
+      </div>
+    </div>
+  );
 }
